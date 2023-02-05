1c0dcc2f6440179bd0616cbe70bbbb1427fab47e
s.gaponov <s.a.gaponov96@gmail.com>
2023-02-05 20:30:00 +0300

    header

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,26 +1,28 @@
-import {
-    Logo, BurgerIcon, ListIcon
-} from '@ya.praktikum/react-developer-burger-ui-components';
+import {Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+
 import styles from './appheader.module.css';
+
 export function AppHeader() {
     return (
         <nav className={styles.nav}>
-            
                 <div className={styles.selectMenu}>
-                    <BurgerIcon/>
-                        <span>Конструктор</span>
-                    <ListIcon type="primary" />
-                        <span>Лента Заказов</span>
+                <div className={styles.constructor} >
+                    <BurgerIcon type="primary"/>
+                    <span>Конструктор</span>
+                </div>
+                <div className={styles.lenta}>
+                <ListIcon type="primary" />
+                <span>Лента Заказов</span>
+                </div>
                 </div>
                 <div className={styles.Logo}>
                         <Logo/>
                 </div>
 
                 <div className={styles.personalCabinet}>
-                    <span>Личный кабинет</span>
+                <ProfileIcon type="primary" />    
+                <span>Личный кабинет</span>
                 </div>
-
-            
         </nav>
     );
 }
